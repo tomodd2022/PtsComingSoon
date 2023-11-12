b36673124381048290797c2bd60c713694d18654
tomodd2022 <tomcom@odds.team>
2023-11-12 11:45:35 +0700

    [ADD] route template-driven-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TemplateDrivenFormComponent } from './components/form-in-angular/template-driven-form/template-driven-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'template-driven-form',component: TemplateDrivenFormComponent }
+];
